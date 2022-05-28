175496e0ede7468242bd5a0bde9fda2160f09cb8
RANJAN KUMAR <91800094+ranjankumar1602@users.noreply.github.com>
2022-05-29 01:00:15 +0530

    Update FLOW018.java

--- FLOW018.java
+++ FLOW018.java
@@ -7,16 +7,17 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner ranjan=new Scanner(System.in);
-		int T=ranjan.nextInt();
+		int T=ranjan.nextInt(); //number of Test Cases
+		//If N=0 the factorial will 1
 		while(T>=1){
 		    int N=ranjan.nextInt();
 		    int fact=1;
 		    if(N==0){
-		        System.out.println(fact);
+		        System.out.println(fact); 
 		    }else{
 		        
 		        for(int i=1;i<=N;i++){
-		            fact*=i;
+		            fact*=i; 
 		        }
 		        System.out.println(fact);
 		    }
